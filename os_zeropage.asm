99f4679662f00c31e32c83ba1a86f780846a4b7b
scirelli <scirelli+git@gmail.com>
2021-12-16 14:16:37 -0500

    example on how to read data from disk

---
+++ os_zeropage.asm
@@ -0,0 +1,6 @@
+; Load address read from input file and pointer to current byte during LOAD/VERIFY from serial bus.
+; End address after LOAD/VERIFY from serial bus or datasette.
+; End address for SAVE to serial bus or datasette.
+; Pointer to line in Color RAM to be scrolled during scrolling the screen.
+ZP_LOAD_ADDR_HI = $AE
+ZP_LOAD_ADDR_LO = $AF
